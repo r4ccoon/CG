64474ba6b084a455875a5266f8632639c0522a4a
ridho <rpl.oye@gmail.com>
2013-12-15 22:43:36 +0100

    almost

--- Assignment6/t1/main.cpp
+++ Assignment6/t1/main.cpp
@@ -2,9 +2,9 @@
 //  main.cpp
 //  Assignment6
 //
-//  Created by Ridho, Rohan, Zeeshan on 6/11/2013.
+//  Created by Ridho, Rohan, Zeeshan
 //
-//
+//  please ctrl+f HS2RGB to find the required implementatsion of the assignment
 
 #ifdef _WIN32
 #include <windows.h>
@@ -20,12 +20,14 @@
 #include <math.h>
 #define PI 3.14159
 #define r360_pi 114.591559026
+#define r180_pi 57.2957795131
 
 #include <cstdio>
 using namespace std;
 
-int width = 360;
-int height = 360;
+int width = 640;
+int height = 480;
+const float _light = 0.5;
 
 // a class of a generic triangle.
 class Circle {
@@ -56,9 +58,9 @@ class Circle {
         }
         
         glEnd();
-        glFlush();
     }
 };
+
 
 class Plane {
     public :
@@ -68,7 +70,7 @@ class Plane {
         L = vL;
     }
     
-    // draw the Circle
+    // draw the Plane
     void Draw(void){
         // set color lines
         glColor3f(0,0,1);
@@ -80,105 +82,291 @@ class Plane {
         glVertex2d( -L, -L);
         
         glEnd();
-        glFlush();
     }
     
     void color(){
-        float step = 1.0f/255;
-        //step = 0.009;
+        float step = L/255;
+        
         glBegin(GL_POINTS);
         float i = 0;
+        
+        float maxAng = 0;
+        float minAng = 0;
+        
         for(float x = -L; x < L; x += step){
             for(float y = -L; y < L; y += step){
-                GLfloat rgb[3] = {0,0,0};
+                int rgb[3] = {0,0,0};
                 
-                double sat = saturation(x, y);
-                float rad = atan2f(y, x);
-                float angle = fabs(rad * r360_pi);
+                double sat = Plane::saturation(x, y, L);
+                float rad = fabs(atan2f(y, x));
+                int angle = fabs(rad * r360_pi);
                 
-                HS2RGB(rad, sat, rgb);
-                glColor3f (rgb[0] , rgb[1] , rgb[2] );
+                // for debug info
+                if(maxAng < angle)
+                    maxAng = angle;
+                if(minAng > angle)
+                    minAng = angle;
+                
+                HS2RGB(angle, sat, rgb);
+                //glColor3b(255 ,53 ,30);
+                glColor3b (rgb[0] , rgb[1] , rgb[2] );
                 
                 //printf("angle = %f  \n", rad);
-                printf("color = %f %f %f \n", rgb[0], rgb[1], rgb[2]);
+                //printf("color = %i %i %i \n", rgb[0], rgb[1], rgb[2]);
                 glVertex2f( x , y );
                 
                 i++;
             }
         }
         
+        // for debug info
+        printf("max angle = %f \n", maxAng);
+        printf("min angle = %f \n", minAng);
         printf("total = %f \n", i);
         
         glEnd();
         glFlush();
     }
     
-    void HS2RGB(float H, float S, GLfloat *rgb){
-         float L = 0.5;
+    static void HS2RGB(float h, float S, int vRGB[]){
+        float r,g,b = 0.0f;
         
-         if ( S == 0 )                       //HSL from 0 to 1
-         {
-             rgb[0] = L * 1;                      //RGB results from 0 to 1
-             rgb[1] = L * 1;
-             rgb[2] = L * 1;
-         }
-         else
-         {
-             float var_2 , var_1 = 0.0f;
-             if ( L < 0.5 )
-                 var_2 = L * ( 1 + S );
-             else
-                 var_2 = ( L + S ) - ( S * L );
-         
-             var_1 = 2 * L - var_2;
+        float L = _light;
+        float C = ( 1 - fabs( 2 * L - 1 ) ) * S;
+        float X = C * ( 1 - fabs( fmod( ( h / 60 ), 2 ) - 1 ) );
+        float M = L - ( C / 2 );
         
-             rgb[0] = 1 * Hue_2_RGB( var_1, var_2, H + ( 1 / 3 ) );
-             rgb[1] = 1 * Hue_2_RGB( var_1, var_2, H );
-             rgb[2] = 1 * Hue_2_RGB( var_1, var_2, H - ( 1 / 3 ) );
-         }        
+        if ( h < 60 ) {
+            r = C;
+            g = X;
+            b = 0;
+        } else if ( h < 120 ) {
+            r = X;
+            g = C;
+            b = 0;
+        } else if ( h < 180 ) {
+            r = 0;
+            g = C;
+            b = X;
+        } else if ( h < 240 ) {
+            r = 0;
+            g = X;
+            b = C;
+        } else if ( h < 300 ) {
+            r = X;
+            g = 0;
+            b = C;
+        } else {
+            r = C;
+            g = 0;
+            b = X;
+        }
+        
+        r = ( r + M ) * 255;
+        g = ( g + M ) * 255;
+        b = ( b + M ) * 255;
+        
+        vRGB[0] = floor( fabs(r) );
+        vRGB[1] = floor( fabs(g) );
+        vRGB[2] = floor( fabs(b) );
     }
     
-    float Hue_2_RGB( float v1, float v2, float vH )
-    {
-        if ( vH < 0 )
-            vH += 1;
+    static float saturation(float x, float y, float Length){
+        float _length = sqrt((x*x) + (y*y));
         
-        if ( vH > 1 )
-            vH -= 1;
-        
-        if ( ( 6 * vH ) < 1 )
-            return ( v1 + ( v2 - v1 ) * 6 * vH );
-        
-        if ( ( 2 * vH ) < 1 )
-            return ( v2 );
-        
-        if ( ( 3 * vH ) < 2 )
-            return ( v1 + ( v2 - v1 ) * ( ( 2 / 3 ) - vH ) * 6 );
-     
-        return ( v1 );
-    }
-    
-    float saturation(float x, float y){
-        float length = sqrt((x*x) + (y*y));
-        
-        float denome = L;
+        float denome = Length;
         // avoid div by 0
-        if(L == 0)
+        if(Length == 0)
             denome = 0.01f;
         
-        return length / denome;
+        return _length / denome;
     }
     
+};
+
+class Square{
+private:
+    int _x, _y;
+    int _x2, _y2;
+    int _mouseClick = 0;
+    GLdouble L;
+    const int _interpolateStep = 150;
+    
+    int _rgb[3];
+    int _rgb2[3];
+    
+    float _hls[3];
+    float _hls2[3];
+    
+    public :
+    
+    Square(GLdouble vL){
+        L = vL;
+    }
+    
+    void MouseClick(int x, int y){
+        _mouseClick ++;
+        
+        if(_mouseClick > 1){
+            _mouseClick = 0;
+        }
+        
+        if(_mouseClick == 1)
+            SetColorPosition(x, y);
+        else
+            SetColorPosition2(x, y);
+    }
+    
+    void color(){
+        color1();
+        color2();
+        colorInterpolateRGB();
+        colorInterpolateHLS();
+        line();
+    }
+    
+private:
+    void SetColorPosition(int x, int y){
+        _x = x;
+        _y = y;
+    }
+    
+    void SetColorPosition2(int x, int y){
+        _x2 = x;
+        _y2 = y;
+    }
+    
+    void line(){
+        glColor3f( 0, 0, 0);
+        glBegin(GL_LINES);
+        glVertex2i(_x, _y);
+        glVertex2i(_x2, _y2);
+        glEnd();
+    }
+    
+    float interpolate(int startValue, int endValue, int lastStepNumber, int stepNumber)
+    {
+        float t = (float)lastStepNumber / stepNumber;
+        return startValue + (endValue - startValue) * t;
+    }
+    
+    void colorInterpolateRGB(){
+        glBegin(GL_POINTS);
+        
+        float start = - width/2;
+        
+        for(float x = start; x < start + L; x ++){
+            int interpolateVar = 1;
+            for(float y = L + 10; y < L + 10 + 150; y ++){
+                int rgb[3] = {0, 0, 0};
+                
+                rgb[0] = interpolate(_rgb[0], _rgb2[0], interpolateVar, _interpolateStep);
+                rgb[1] = interpolate(_rgb[1], _rgb2[1], interpolateVar, _interpolateStep);
+                rgb[2] = interpolate(_rgb[2], _rgb2[2], interpolateVar, _interpolateStep);
+                
+                glColor3b (rgb[0] , rgb[1] , rgb[2] );
+                glVertex2f( x , y );
+                
+                interpolateVar++;
+            }
+        }
+        glEnd();
+    }
+    
+    void colorInterpolateHLS(){
+        glBegin(GL_POINTS);
+        
+        float start = - width/2;
+        for(float x = start + L; x < start + L + L; x ++){
+            int interpolateVar = 1;
+            for(float y = L + 10; y < L + 10 + 150; y ++){
+                float hls[3] = {0.0f, 0.0f, 0.0f};
+                int rgb[3] = {0,0,0};
+                
+                hls[0] = interpolate(_hls[0], _hls2[0], interpolateVar, _interpolateStep);
+                hls[1] = interpolate(_hls[1], _hls2[1], interpolateVar, _interpolateStep);
+                hls[2] = interpolate(_hls[2], _hls2[2], interpolateVar, _interpolateStep);
+                
+                Plane::HS2RGB(hls[0], hls[1], rgb);
+                
+                glColor3b (rgb[0] , rgb[1] , rgb[2] );
+                glVertex2f( x , y );
+                
+                interpolateVar++;
+            }
+        }
+        printf("coord = %i %i \n", _x, _y);
+        glEnd();
+    }
+    
+    void color1(){
+        glBegin(GL_POINTS);
+        
+        float start = - width/2;
+        for(float x = start; x < start + L; x ++){
+            for(float y = 0; y < L; y ++){
+                int rgb[3] = {0, 0, 0};
+                
+                double sat = Plane::saturation(_x, _y, L);
+                float rad = fabs(atan2f(_y, _x));
+                float angle = fabs(rad * r360_pi);
+                
+                Plane::HS2RGB(angle, sat, rgb);
+                
+                _rgb[0] = rgb[0];
+                _rgb[1] = rgb[1];
+                _rgb[2] = rgb[2];
+                
+                _hls[0] = angle;
+                _hls[1] = sat;
+                _hls[2] = _light;
+
+                glColor3b (rgb[0] , rgb[1] , rgb[2] );
+                glVertex2f( x , y );
+            }
+        }
+        printf("coord = %i %i \n", _x, _y);
+        glEnd();
+    }
+    
+    void color2(){
+        glBegin(GL_POINTS);
+        
+        float start = - width/2;
+        for(float x = start + L; x < start + L + L; x ++){
+            for(float y = 0; y < L; y ++){
+                int rgb[3] = {0, 0, 0};
+                
+                double sat = Plane::saturation(_x2, _y2, L);
+                float rad = fabs(atan2f(_y2, _x2));
+                float angle = fabs(rad * r360_pi);
+                
+                Plane::HS2RGB(angle, sat, rgb);
+                
+                _rgb2[0] = rgb[0];
+                _rgb2[1] = rgb[1];
+                _rgb2[2] = rgb[2];
+                
+                _hls2[0] = angle;
+                _hls2[1] = sat;
+                _hls2[2] = _light;
+                
+                glColor3b (rgb[0] , rgb[1] , rgb[2] );
+                glVertex2f( x , y );
+            }
+        }
+        
+        printf("coord2 = %i %i \n", _x2, _y2);
+        glEnd();
+    }
 };
 
 void init(void){
     glClearColor(1.0, 1.0 , 1.0, 1.0);
     glViewport (0, 0, (GLsizei) width, (GLsizei) height);
-    
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    float wh = (float)height/width;
-    glOrtho(-1, 1, -wh, wh , -1.0f, 1.0f);
+    glOrtho(-width/2, width/2, -height/2, height/2 , -1.0f, 1.0f);
 }
 
 void reshape(int w, int h){
@@ -186,30 +374,50 @@ void reshape(int w, int h){
     height = h;
     
     glViewport (0, 0, (GLsizei) w, (GLsizei) h);
-    
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    float wh = (float)height/width;
-    glOrtho(-1, 1, -wh, wh , -1.0f, 1.0f);
+    glOrtho(-width/2, width/2, -height/2, height/2 , -1.0f, 1.0f);
 }
 
 // initiate the triangle that we want to draw
-Circle theCircle = Circle(0.5, 64);
-Plane thePlane = Plane(0.5);
+Circle theCircle = Circle(100, 64);
+Plane thePlane = Plane(100);
+Square pickedColor = Square(40);
 
 // method to be passed to glutdisplayfunc to draw the triangle lines and colors
 void draw(void){
     glClear(GL_COLOR_BUFFER_BIT);
+    
     theCircle.Draw();
     thePlane.color();
+    pickedColor.color();
+    
+    glFlush();
+}
+
+void mouseMap(int button, int state, int x, int y)
+{
+	if (button == GLUT_LEFT_BUTTON)
+	{
+		if (state == GLUT_DOWN)
+		{
+            pickedColor.MouseClick(x, y);
+        }
+	}
+    
+    glutPostRedisplay();
+}
+
+void mouseDrag(int x, int y)
+{
+	pickedColor.MouseClick(x, y);
+    glutPostRedisplay();
 }
 
 int main( int argc, char* argv[] )
 {
 	glutInit(&argc, argv);
     
-    printf("anglea = %f  \n", atan2(-1, -1));
-
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowPosition(10,10);
     glutInitWindowSize(width, height);
@@ -219,6 +427,10 @@ int main( int argc, char* argv[] )
     
     glutDisplayFunc(draw);
     glutReshapeFunc(reshape);
+	glutMotionFunc(mouseDrag);
+    
+	glutMouseFunc(mouseMap);
+    
     glutMainLoop();
     
 	return 0;
